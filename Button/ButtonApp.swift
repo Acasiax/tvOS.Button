4874c1f94ba8077e2e6d1b72b5c8228fd841510e
Acasiax <117105267+Acasiax@users.noreply.github.com>
2023-12-15 22:23:10 +0900

    Initial Commit

---
+++ Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by 이윤지 on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Button/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Button
+//
+//  Created by 이윤지 on 12/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
